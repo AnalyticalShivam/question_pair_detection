36e01fe522fbb66cf7a4b44dc35c5d25e9a624c1
AnalyticalShivam <ssg98670@gmail.com>
2023-10-22 18:16:21 +0530

    update

--- app.py
+++ app.py
@@ -8,12 +8,14 @@ st.header('Duplicate Question Pairs')
 
 q1 = st.text_input('Enter question 1')
 q2 = st.text_input('Enter question 2')
+try:
+    if st.button('Find'):
+        query = helper.query_point_creator(q1,q2)
+        result = model.predict(query)[0]
 
-if st.button('Find'):
-    query = helper.query_point_creator(q1,q2)
-    result = model.predict(query)[0]
-
-    if result:
-        st.header('Duplicate')
-    else:
-        st.header('Not Duplicate')
+        if result:
+            st.header('Duplicate')
+        else:
+            st.header('Not Duplicate')
+except Exception as e:
+    print('an error occured try another question')
